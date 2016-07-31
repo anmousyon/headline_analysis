353d34b71873e93f68f6e870d04e730e4bc65505
anmousyon <anmousyon@gmail.com>
2016-07-31 00:04:55 -0500

    few updates for fun
    explanation is mostly in the comments (feel free to delete those, they dont really belong in the application, i just didnt want to make a bunch of different commits). It should be more extensible now, and would be even more so with if you wanted to create a small sqlite3 database to store the posts. You should look into searching the top level comments to find their sentiment and see how that matches the post title's. 
    
    I did all the coding through github's editor so there may be some syntax or indentation errors.

--- reddit_titles.py
+++ reddit_titles.py
@@ -1,39 +1,92 @@
 #!/usr/bin/env python
 
 import argparse
-import json
 import sys
-import urllib
 
 try:
     import praw
 except ImportError:
     print("You must 'sudo pip install praw'")
     sys.exit(1)
+try:
+    from textblob import TextBlob
+except ImportError:
+    print("You must 'sudo pip install textblob'")
+    sys.exit(1)
 
-def sentiment(text):
-    try:
-        data = urllib.urlencode({"text": text})
-    except Exception as e:
-        print("Warning: %s" % e)
-        print(text)
-        return ""
-    u = urllib.urlopen("http://text-processing.com/api/sentiment/", data)
-    the_page = json.load(u)
-    return the_page['label']
+def get_sentiment(text):
+    ''
+    textblob seems to be more accurate for sentiment analysis
+    you also won't get a ratelimit/slowdown since its all local
+    '''
+    sentiment = TextBlob(text).sentiment.polarity
+    #I'm using the polarity, and manually separating it
+    if sentiment > 0.2:
+        return "postive"
+    elif sentiment < -0.2:
+        return "negative"
+    return "neutral"
+
+def check_subs(reddit, subs):
+    '''
+    i took away the limit, just because unless youre doing something special
+    its annoying to have argeparse just for it since it shouldnt change much
+    '''
+    
+    #added a subreddit sentiment average to compare different subreddits automatically
+    sub_sents = []
+
+    for sub in subs:
+        #changed some variable names because i like being extra clear, you dont need to keep them
+        posts = reddit.get_subreddit(args.subreddit).get_top(limit=10)
+        titles = [post.title.encode('utf-8') for post in posts]
+        total = 0
+        for title in titles:
+            sentiment = get_sentiment(title)
+            print("{:<8} {:<10}".format(sentiment, title))
+            total += sentiment
+        sub_sents.append(total/len(posts))
+    
+    #merge the two lists into a dict
+    avg_sents = {}
+    for sub, sent in zip(subs, sub_sents):
+        avg_sent[sub] = sent
+    return avg_sents
+
+def extremes(avg_sents):
+    #im not sure if this works well, and its definitely written poorly, but you get the idea. just for fun
+    #feel free to remove this if it doesnt work properly
+    neg_key = avg_sents.keys()[0]
+    neg = avg_sents.values()[0]
+    pos_key = neg_key
+    pos = neg
+    for key, val in avg_sents:
+        if val < neg:
+            neg_key = key
+            neg = val
+        elif val > pos:
+            pos_key = key
+            pos = val
+    return [neg_key, neg], [pos_key, pos]
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get Reddit Titles')
-    parser.add_argument('subreddit', type=str,
-                        help='Name of subreddit, e.g. "news"')
-    parser.add_argument('-n', type=int, default=10,
-                        help='Number of titles')
-    args = parser.parse_args()
+    #using a file for the subreddits allows you to check multiple subreddits easier
+    with open('subreddits.txt') as file:
+        #for subreddits.txt, just put a single sub name on each line with no whitespace other than \n
+        subs = file.readlines()
 
-    r = praw.Reddit(user_agent='Test Script by /u/goodDayM')
-    submissions = r.get_subreddit(args.subreddit).get_top(limit=args.n)
-    titles = [x.title.encode('utf-8') for x in submissions]
-    for t in titles:
-       s = sentiment(t)
-       print("{:<8} {:<10}".format(s, t))
+    #using a version number helps reddit stop from considering you a spam bot, theyre really picky
+    reddit = praw.Reddit(user_agent='Test Script v.01 by /u/goodDayM')
+    
+    #get a dictionary with the average sentiment for each sub (is /r/news negative? lets see!)
+    avg_sents = check_subs(reddit, subs)
+    
+    #find and print the most extreme subs (most positive and most negative)
+    pos, neg = extremes(avg_sents)
+    print('pos:', pos)
+    print(neg:', neg)
+    
+    #check out the all the results
+    print(avg_sents)
+    
 
